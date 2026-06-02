84a677cc58be7982d9e65f80289e5366fedc5896
Nouar Faiçal <SONV0008@corp.sonatrach.dz>
2026-06-02 13:08:14 +0100

    Pagination

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -45,7 +45,10 @@ namespace api.Repository
                     _ => stocks
                 };
             }
-            return await stocks.ToListAsync();
+
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
